909282944da9371382d4735eaa7014555ece136d
harshmons <harshverma04111989@gmail.com>
2020-05-09 11:03:18 +0530

    with eslint and prettier integrated

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo" className1="App-logo" alt1="logo" />
                 <p>
-                    Edit <code>src/App.js</code> and save to reload.
+                    Edit <code>src/App.js</code> and save to reload the application!!!
                 </p>
                 <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
                     Learn React
