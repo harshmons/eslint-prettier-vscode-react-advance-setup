a871bcf36b618f665063f495fd0f7680832d5ecb
harshmons <harshverma04111989@gmail.com>
2020-05-10 13:56:00 +0530

    Added testing libraries

--- src/DummyComponent.test.js
+++ src/DummyComponent.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import App from './App';
+import DummyComponent from './DummyComponent';
 import Enzyme, { shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 Enzyme.configure({ adapter: new Adapter() });
-const enzymeWrapper = shallow(<App />);
+const enzymeWrapper = shallow(<DummyComponent />);
 describe('<App />', () => {
     test('should render successfully', () => {
         expect(enzymeWrapper).toBeDefined();
